d4049486ea41374e1e6a4e46fea38c5ae0a5f9eb
Kornberger, Ralf <Ralf.Kornberger@kk-service.de>
2023-09-05 08:39:51 +0200

    Create 2023-09-05-061244_AlterTableUsers.php

---
+++ src/Database/Migrations/2023-09-05-061244_AlterTableUsers.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Rakoitde\Shieldldap\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AlterTableUsers extends Migration
+{
+    public function up()
+    {
+
+        $fields = [
+            'mail'            => ['type' => 'VARCHAR', 'constraint' => 50, 'after' => 'username'],
+            'object_sid'      => ['type' => 'VARCHAR', 'constraint' => 50, 'after' => 'mail'],
+            'dn'              => ['type' => 'VARCHAR', 'constraint' => 50, 'after' => 'object_sid'],
+            'ldap_attributes' => ['type' => 'TEXT', 'after' => 'dn'],
+            'ldap_group_sids' => ['type' => 'TEXT', 'after' => 'ldap_attributes'],
+        ];
+
+        $this->forge->addColumn('users', $fields);
+
+    }
+
+    public function down()
+    {
+
+        $this->forge->dropColumn('users', ['mail', 'object_sid', 'dn', 'ldap_attributes', 'ldap_group_sids']);
+
+    }
+}
