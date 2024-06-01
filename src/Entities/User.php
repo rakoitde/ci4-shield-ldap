08f25d1c8b50f15e933ae3b030fa5a21400da9c4
Kornberger, Ralf <Ralf.Kornberger@kk-service.de>
2024-06-01 09:28:22 +0200

    feat: User entity

---
+++ src/Entities/User.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rakoitde\Shieldldap\Entities;
+
+use App\Models\CompanyModel;
+use CodeIgniter\Shield\Entities\User as ShieldUserEntity;
+use stdClass;
+use Rakoitde\Shieldldap\Authentication\LDAPManager;
+
+class User extends ShieldUserEntity
+{
+    public function ldapAttributes(): stdClass
+    {
+        if (null === $this->ldap_attributes) {
+            return new stdClass();
+        }
+
+        try {
+            $ldapAttributes = json_decode($this->ldap_attributes);
+        } catch (Exception $e) {
+            return new stdClass();
+        }
+
+        return $ldapAttributes;
+    }
+
+    public function ldapAttribute(string $attribute): string
+    {
+        if (null === $this->ldap_attributes) {
+            return '';
+        }
+
+        try {
+            $ldapAttributes = json_decode($this->ldap_attributes);
+        } catch (Exception $e) {
+            return '';
+        }
+
+        return $ldapAttributes->{$attribute} ?? '';
+    }
+
+    /**
+     * Check if user account is disabled
+     */
+    public function isLdapAccountDisabled(): ?bool
+    {
+
+        if ( $this->ldapAttribute('userAccountControl') == '' ) {
+            return null;
+        }
+
+        return (intval($this->ldapAttribute('UserAccountControl')) & LDAPManager::UAC_ACCOUNTDISABLE) == LDAPManager::UAC_ACCOUNTDISABLE;
+
+    }
+
+    /**
+     * Check if user account is disabled
+     */
+    public function isLdapAccountEnabled(): ?bool
+    {
+
+        if ( $this->ldapAttribute('userAccountControl') == '' ) {
+            return null;
+        }
+
+        return !((intval($this->ldapAttribute('UserAccountControl')) & LDAPManager::UAC_ACCOUNTDISABLE) == LDAPManager::UAC_ACCOUNTDISABLE);
+
+    }
+}
